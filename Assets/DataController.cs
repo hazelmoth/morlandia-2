cd34579471629bdd573c97d875e2e24b62834859
Linus Waddell <linuswaddell@gmail.com>
2017-11-24 16:18:32 -0800

    Allow addition of characters in the map .csv file
    Characters can now be added to maptiles by appending @<character name>
    to the description.

--- Assets/DataController.cs
+++ Assets/DataController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.IO;
+using System.Text.RegularExpressions;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.UI;
@@ -20,17 +21,50 @@ public class DataController : MonoBehaviour {
 		// Declare an array to store just the descriptions, and initialize mapArray to store the whole maptiles
 
 		string[,] mapDescriptions = new string[mapX, mapY];
-		mapArray = new MapTile[mapX, mapY];
 
-		// Start a streamreader to read each line and split it into pieces, then store each piece in the description array
+		mapArray = new MapTile[mapX, mapY];
+		for (int x = 0; x < mapX; x++) {
+			for (int y = 0; y < mapY; y++) {
+				mapArray[x, y] = new MapTile(); // Initialize every map tile in preparation for storing things in them
+			}
+		}
+
+		// Start a streamreader to read each line and split it into pieces, then store each piece in the description array.
+		// Each line in the map file is a row (y), and each section of the row, divided by '|', is an x.
+		// We're using mapDescriptions to temporarily grab the descriptions from the map file, before we copy them into the final map array (mapArray).
 
 		StreamReader streamReader = new StreamReader (filePath);
 		int row = 0;
 		while (!streamReader.EndOfStream) {
 			string[] line = streamReader.ReadLine ().Split ('|');
-			for (int i = 0; i < mapX; i++) {
+
+			for (int i = 0; i < mapX; i++) {	// Loop through all the descriptions on a line; we're assuming there will be the same number as mapX
 				Debug.Log (line [i]);
-				mapDescriptions [i, row] = line [i];
+
+				if (mapArray[i, row].Characters == null) {
+					mapArray[i, row].Characters = new List<Character>(); // Create a list of characters on the current tile if there isn't one
+				}
+
+
+				// Check if the current description contains the '@' character, and if it does, use Regex to find each name following an @ character.
+
+				if (line[i].Contains('@'))
+				{
+					foreach (Match matchedCharacterName in Regex.Matches(line[i], "(?<=@)[\\w ]+"))
+					{
+						string nameString = matchedCharacterName.ToString();
+						nameString.Trim();
+						mapArray[i, row].Characters.Add((Character)AssetDatabase.LoadAssetAtPath<ScriptableObject>("Assets/Data/Characters/" + nameString.Trim() + "/character.asset"));
+					}
+					mapDescriptions[i, row] = line[i].Substring(0, line[i].IndexOf('@')).Trim(); // Add the description, excluding the names, to the mapDescriptions array.
+				}
+
+				// If there are no @ characters, just copy the string into mapDescriptions.
+
+				else 
+				{
+					mapDescriptions[i, row] = line[i].Trim();
+				}
 			}
 			row++;
 		}
@@ -39,18 +73,9 @@ public class DataController : MonoBehaviour {
 
 		for (int x = 0; x < mapX; x++) {
 			for (int y = 0; y < mapY; y++) {
-				mapArray[x, y] = new MapTile();
 				mapArray[x, y].Description = mapDescriptions[x, y];
 			}
 		}
-
-		// For testing
-
-		if (mapArray[0,0].Characters == null)
-		{
-			mapArray[0, 0].Characters = new List<Character>();
-		}
-		mapArray[0,0].Characters.Add((Character)AssetDatabase.LoadAssetAtPath<ScriptableObject>("Assets/Data/Characters/Sundar/character.asset"));
 	}
 }
 
